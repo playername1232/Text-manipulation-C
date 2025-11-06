a43d8696ab47b530887b164c271ede107ca159f5
Martin Krepčík <martinkrepcik@gmail.com>
2025-11-06 11:26:51 +0100

    WIP changes

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct WordChanges
 {
@@ -54,7 +56,7 @@ char asciiToUpperCase(char _c)
 // endregion
 
 // region Text manipulation
-int containsIdenticalCharactersInRow(const char* str, const unsigned int* length)
+int ContainsIdenticalCharactersInRow(const char* str, const unsigned int* length)
 {
     if (*length == 1)
     {
@@ -72,7 +74,7 @@ int containsIdenticalCharactersInRow(const char* str, const unsigned int* length
     return false;
 }
 
-int toUpperStr(char* str, const unsigned int* length)
+int ToUpperStr(char* str, const unsigned int* length)
 {
     int changed = 0;
 
@@ -85,18 +87,39 @@ int toUpperStr(char* str, const unsigned int* length)
     return changed;
 }
 
-void removeDuplicitSpaces(char* str, const unsigned int* length, struct WordChanges* wordChanges)
+void TrimSpacesStart(char* str, struct WordChanges* wordChanges)
 {
-    int increment = 0;
+    int switchIndex = 0;
 
-    while (str[increment++] == ' ');
-
-    if (--increment <= 0)
+    for (int i = 0; str[i] != '\0' && str[i] == ' '; i++)
     {
-        increment = 1;
+        switchIndex++;
     }
 
-    for (unsigned int i = 0; (i + increment) < *length; i++)
+    for (int i = 0;  str[i] != '\0'; i++)
+    {
+        str[i] = str[i + switchIndex];
+    }
+
+    wordChanges->spacesBefore += switchIndex;
+}
+
+void TrimSpacesEnd(char* str, struct WordChanges* wordChanges)
+{
+    int len = strlen(str);
+
+    for (int i = len - 1; i >= 0 && str[i] == ' '; i--)
+    {
+        str[i] = str[i + 1];
+        wordChanges->spacesBefore++;
+    }
+}
+
+void RemoveDuplicitSpaces(char* str, struct WordChanges* wordChanges)
+{
+    int increment = 1;
+
+    for (unsigned int i = 0; str[i + increment] != '\0'; i++)
     {
         if (str[i] == ' ' && str[i + increment] == ' ')
         {
@@ -110,68 +133,115 @@ void removeDuplicitSpaces(char* str, const unsigned int* length, struct WordChan
     }
 }
 
-void normalizeFirstLetterCapital(char** str, const unsigned int* length, struct WordChanges* wordChanges)
+void NormalizeWordFirstLetterCapital(char* str, const unsigned int* start, struct WordChanges* wordChanges)
 {
-    int bFirstLetter = true;
-
-    for (unsigned int i = 0; i < *length; i++)
+    if (asciiIsLowerCase(str[*start]))
     {
-        if (*str[i] == '_')
-        {
-            bFirstLetter = true;
-            wordChanges->spacesBefore++;
-            wordChanges->spacesAfter++;
-            continue;
-        }
+        str[*start] = asciiToUpperCase(str[*start]);
+        wordChanges->lowerCaseBefore++;
+    }
+    else
+    {
+        wordChanges->upperCaseBefore++;
+    }
 
-        if (bFirstLetter)
-        {
-            if (asciiIsLowerCase(*str[i]))
-            {
-                wordChanges->lowerCaseBefore++;
-                *str[i] = asciiToUpperCase(*str[i]);
-            }
-            else
-            {
-                wordChanges->upperCaseBefore++;
-            }
+    wordChanges->upperCaseAfter++;
 
-            wordChanges->upperCaseAfter++;
-            bFirstLetter = false;
+    for (unsigned int i = *start + 1; str[i] != ' ' && str[i] != '\0'; i++)
+    {
+        if (asciiIsUpperCase(str[i]))
+        {
+            str[i] = asciiToLowerCase(str[i]);
+            wordChanges->upperCaseBefore++;
         }
         else
         {
-            if (asciiIsUpperCase(*str[i]))
-            {
-                wordChanges->upperCaseBefore++;
-                *str[i] = asciiToLowerCase(*str[i]);
-            }
-            else
-            {
-                wordChanges->lowerCaseBefore++;
-            }
+            wordChanges->lowerCaseBefore++;
+        }
 
-            wordChanges->lowerCaseAfter++;
+        wordChanges->lowerCaseAfter++;
+    }
+}
+
+void PerformWordCheck(char* ptr, const unsigned int* start, struct WordChanges* wordChanges)
+{
+    NormalizeWordFirstLetterCapital(ptr, start, wordChanges);
+}
+
+void PrintWord(const char* str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        printf("%c", str[i]);
+    }
+}
+
+void PrintChanges(struct WordChanges* wordChanges)
+{
+    printf("\nlowercase: %d -> %d\n", wordChanges->lowerCaseBefore, wordChanges->lowerCaseAfter);
+    printf("uppercase: %d -> %d\n", wordChanges->upperCaseBefore, wordChanges->upperCaseAfter);
+    printf("spaces: %d -> %d\n", wordChanges->spacesBefore, wordChanges->spacesAfter);
+}
+
+void PerformStringCheck(char* ptr, struct WordChanges* wordChanges)
+{
+    TrimSpacesStart(ptr, wordChanges);
+    TrimSpacesEnd(ptr, wordChanges);
+
+    unsigned int startIdx = 0;
+
+    for (int i = 0; ptr[i] != '\0'; i++)
+    {
+        if (ptr[i] == ' ')
+        {
+            PerformWordCheck(ptr, &startIdx, wordChanges);
+            startIdx = i + 1;
+            wordChanges->spacesBefore++;
+            wordChanges->spacesAfter++;
         }
     }
+
+    NormalizeWordFirstLetterCapital(ptr, &startIdx, wordChanges);
+
+    printf("\n");
+    PrintWord(ptr);
+    PrintChanges(wordChanges);
 }
 
 // endregion
 
 int main(void)
 {
-    char test[10] = { ' ', ' ', 'a', 'v', ' ', ' ', 'f', 'a', 'c', '\0' };
+    char numBuffer[32];
 
-    struct WordChanges changes = { 0, 0, 0, 0, 0, 0 };
+    fgets(numBuffer, sizeof(numBuffer), stdin);
 
-    const unsigned int len = 10;
+    unsigned int numberOfWords = (unsigned int)strtoul(numBuffer, NULL, 10);
 
-    removeDuplicitSpaces(&test[0], &len, &changes);
-
-    for (int i = 0; i < 10; i++)
+    char (*words)[50] = malloc(numberOfWords * sizeof(*words));
+    if (words == NULL)
     {
-        printf("'%c' ", test[i]);
+        return -1;
     }
 
+    for (unsigned int i = 0; i < numberOfWords; i++)
+    {
+        fgets(words[i], sizeof(words[i]), stdin);
+    }
+
+    struct WordChanges* changes = calloc(numberOfWords, sizeof(struct WordChanges));
+    if (changes == NULL)
+    {
+        free(words);
+        return -1;
+    }
+
+    for (int i = 0; i < numberOfWords; i++)
+    {
+        PerformStringCheck(words[i], &changes[i]);
+    }
+
+    free(words);
+    free(changes);
     return 0;
 }
